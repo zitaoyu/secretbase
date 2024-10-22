2983b8879fc33d87a3297857c8a99124916d0208
zitaoyu <zitaoyu.work@gmail.com>
2024-10-21 20:46:26 -0700

    fix

--- src/app/_components/PokemonPage/DetailPanel.tsx
+++ src/app/_components/PokemonPage/DetailPanel.tsx
@@ -39,31 +39,6 @@ const splitParagraph = (text: string): string[] => {
   return result.filter(Boolean); // Filter out any empty strings
 };
 
-const splitParagraph = (text: string): string[] => {
-  // Define an array of keywords that should trigger a split
-  const keywords = ["Overworld:", "The following are unaffected"];
-
-  // Create a dynamic regex to match any of the keywords
-  const regex = new RegExp(`\\b(${keywords.join("|")})`, "g");
-
-  // Split the text at the keywords, keeping the keyword in the split result
-  const paragraphs = text.split(regex);
-
-  // Recombine the keyword and the following text into paragraphs
-  const result = [];
-  for (let i = 0; i < paragraphs.length; i++) {
-    // If this part is a keyword, combine it with the next part
-    if (keywords.includes(paragraphs[i])) {
-      result.push(`${paragraphs[i]} ${paragraphs[i + 1]}`.trim());
-      i++; // Skip the next part because it's already combined
-    } else {
-      result.push(paragraphs[i].trim());
-    }
-  }
-
-  return result.filter(Boolean); // Filter out any empty strings
-};
-
 interface DetailPanelProps {
   detailPanelData: DetailPanelData;
   setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
@@ -85,16 +60,13 @@ export const DetailPanel = ({
         {/* TODO: implement UI */}
         <Card className="flex h-full w-full p-4">
           <CardHeader className="flex justify-end p-0 sm:p-4">
-          <CardHeader className="flex justify-end p-0 sm:p-4">
             <PrimaryButton
               className="text-lg font-normal"
               onClick={() => setIsOpen(false)}
             >
               Close
-              Close
             </PrimaryButton>
           </CardHeader>
-          <CardHeader className="flex justify-center p-0 pb-1 sm:p-4">
           <CardHeader className="flex justify-center p-0 pb-1 sm:p-4">
             <h1 className="text-4xl font-medium">
               {capitalizeFirstLetter(detailPanelData.type)}
